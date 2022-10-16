1f15ee5d32ef8c44531f4fe53eb5ecebdb6dc9bd
Maxim Danilov <danmaxdanilov@gmail.com>
2022-10-16 12:58:46 +0300

    remove to zts.gateway

--- internal/products/features/getting_product_by_id/dtos/get_product_by_id_response.go
+++
@@ -1,9 +0,0 @@
-package dtos
-
-import (
-	"github.com/danmaxdanilov/zts.writer/internal/products/dtos"
-)
-
-type GetProductByIdQueryResponse struct {
-	Product *dtos.ProductDto `json:"product"`
-}
